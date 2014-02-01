a5d67275fa63c75b2281d14af90946a4a44d1c6d
Evan McCullough <webmaster@evanamccullough.com>
2014-02-01 14:14:03 -0500

    Revert 98c4d00..89ed3f0
    This rolls back to commit 98c4d00f52cb2b8763b348cd11ab334ddfea9691.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,9 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
 //= require jquery-ui.custom.min
 //= require jquery.min
+//= require jquery_ujs
 //= require turbolinks
 //= require bootstrap
 //= require_tree .
