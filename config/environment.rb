44df172e7e0d98b1c272f58c24fde87825678dc0
Evan McCullough <webmaster@evanamccullough.com>
2014-02-03 11:25:59 -0500

    removed line from env rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Todo::Application.initialize!
 
-config.gem 'javan-whenever', :lib => false, :source => 'http://gems.github.com'
 
