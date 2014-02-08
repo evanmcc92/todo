d19a5d420119eda1066099c7d5b0db893e89cd36
Evan McCullough <webmaster@evanamccullough.com>
2014-02-08 13:36:05 -0500

    mailer

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,17 +5,17 @@ class Task < ActiveRecord::Base
   validate :description,  length: {maximum: 2500}
 
   def self.check_reminder
-    current_date = Time.now.strftime("%d/%m/%Y %H:%M").to_s
+    current_date = Time.now
     self.where(:reminder => current_date).each do |task|
       ModelMailer.task_reminder(task).deliver
   	end
   end
 
   def self.check_due
-    current_date = Time.now.strftime("%d/%m/%Y %H:%M").to_s
+    current_date = Time.now
     self.where(:due => current_date).each do |task|
       ModelMailer.task_due(task).deliver
     end
-  end 
+  end
 
 end
